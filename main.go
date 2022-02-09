40beb95b548e220e425267adfdbce18d53364cd4
techagentng <techagentng@gmail.com>
2022-02-09 03:51:22 +0100

    User modey

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+type Log struct {
+	mu mutex
+}
+
+func main() {
+
+}
+
+//commit logs is a sequence of objects(logs) with a unique key used to identify th
+//log stash
---
+++ models/userModel.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type User struct{
+	ID	primitive.ObjectID	`bson:"_id"`
+	First_name	*string 	`json:"first_name" validate:"required, min=2, max=100"`
+	Last_name	*string		`json:"last_name" validate:"required, min=6, max=100"`
+	password	*string	    `json:"last_name" validate:"required, min=6"`
+	Email	*string			`json:"last_name" validate:"required"`
+	Phone	*string         `json:"last_name" validate:"required"`
+	Token *string			`json:"token"`
+	User_type *string        `json:"user_type" validate:"required, eq=ADMIN|eq=USER"`
+	Refresh_token *string    `json:"refresh_token"`
+	Created_at time.Time     `json:"created_at"`
+	Updated_at time.Time	 `json:"updated_at"`
+	User_id	string			  `json:"user_id"`
+}
