389adbed574e022e5f2763a87e2b29d8a7a66984
techagentng <techagentng@gmail.com>
2022-03-08 16:57:12 +0100

    add user controller

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,44 +2,106 @@ package controllers
 
 import (
 	"context"
-	"github.com/techagentng/GOAUTH-JJWT/database"
-
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
-	"go.mongodb.org/mongo-driver/mongo"
-	"golang.org/x/crypto/bcrypt"
-	helper "github.com/techagentng/GOAUTH-JJWT/helpers"
+	"github.com/techagentng/GOAUTH-JJWT/database"
+	"github.com/techagentng/GOAUTH-JJWT/helpers"
 	"github.com/techagentng/GOAUTH-JJWT/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	_ "golang.org/x/crypto/bcrypt"
+	_ "strconv"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 var validate = validator.New()
 
-func Signup(){
+func Signup() gin.HandlerFunc {
+	return func(c *gin.Context){
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
 
-}
-func Login(){
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		validateErr := validate.Struct(user)
+		if validateErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validateErr.Error()})
+		}
 
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panicln(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while checking for user email"})
+		}
+		count, err = userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panicln(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while checking for the phone number"})
+		}
+		if count > 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "this email or phone number already exist"})
+		}
+		user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.User_id = user.ID.Hex()
+		token, refreshToken, _ := helpers.GenerateAllTokens(*user.Email, *user.FirstName, *user.LastName, *user.UserType, *&user.User_id)
+		user.Token = &token
+		user.RefreshToken = &refreshToken
+
+		resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
+		if insertErr != nil {
+			msg := fmt.Sprintf("User item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, resultInsertionNumber)
+	}
 }
+
+func Login() gin.HandlerFunc{
+	return func(c *gin.Context){
+		var ctx, cancel =  context.WithTimeout(context.Background(), 100 * time.Second)
+		var user models.User
+	}
+}
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context){
+		//userId := c.Param("user_id")
+	}
+}
+
 func GetUser() gin.HandlerFunc {
 	return func(c *gin.Context){
 		userId := c.Param("user_id")
-		helper.
+		if err := helpers.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"Error": err.Error()}) //parse error to header
+			return
+		}
+		c.JSON(http.StatusOK, user)
 	}
 }
-func GetUserID()  {
 
-}
+func HashPassword() {}
 
-func HashPassword(){
-
-}
-
-func VerifyPassword()  {
-
-}
+func VerifyPassword() {}
