3fdb5a743ad07a621561fb9a1bc04d0366718a8c
techagentng <techagentng@gmail.com>
2022-03-08 17:01:27 +0100

    add user routes

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -9,5 +9,5 @@ import (
 func UserRoutes(incomingRoutes *gin.Engine){
 	incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.GET("/users", controller.GetUsers())
-	incomingRoutes.GET("/users/:user_id", controller.GetUserID())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
 }
